520bbec4ea67892d6e087dd85d611ac5c9755e6b
Fabian Reinhard <fabian.reinhard@seantis.ch>
2010-01-06 23:54:03 +0000

    use a master template

--- seantisinvoice/views.py
+++ seantisinvoice/views.py
@@ -1,4 +1,6 @@
 from repoze.bfg.chameleon_zpt import render_template_to_response
+from repoze.bfg.chameleon_zpt import render_template
+from repoze.bfg.chameleon_zpt import get_template
 
 from seantisinvoice.models import DBSession
 
@@ -11,7 +13,9 @@ def add_customer(request):
                                        
 def invoices(request):
     dbsession = DBSession()
+    main = get_template('templates/master.pt')
 
     return render_template_to_response('templates/invoices.pt',
+                                       main=main,
                                        request=request)
     
