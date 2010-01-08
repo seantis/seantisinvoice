c40256c39717c69ce54d02b5b4c8bcdbc74f601f
Tobias Reinhard <tobias.reinhard@seantis.ch>
2010-01-08 11:04:25 +0000

    Customer edit form.

--- seantisinvoice/views/customer.py
+++ seantisinvoice/views/customer.py
@@ -3,6 +3,7 @@ from webob.exc import HTTPFound
 import schemaish
 from validatish import validator
 
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.util import class_mapper
 
 from repoze.bfg.url import route_url
@@ -20,32 +21,64 @@ address1_field = schemaish.String(
        description=('Address 1'),
        validator=validator.Required(),
        )
+       
+city_field = schemaish.String(
+       description=('City'),
+       validator=validator.Required(),
+       )
 
-class AddCustomerController(object):
+class CustomerController(object):
     
     def __init__(self, context, request):
         self.request = request
         
     def form_fields(self):
         fields = [ ('name', name_field),
-                   ('address1', address1_field) ]
+                   ('address1', address1_field),
+                   ('city', city_field) ]
         return fields
+        
+    def form_defaults(self):
+        
+        defaults = {}
+        if "customer" in self.request.matchdict:
+            customer_id = self.request.matchdict['customer']
+            try:
+                session = DBSession()
+                customer = session.query(Customer).filter_by(id=customer_id).one()
+            except NoResultFound:
+                return HTTPFound(location = route_url('customers', self.request))  
+            field_names = [ p.key for p in class_mapper(Customer).iterate_properties ]
+            form_fields = [ field[0] for field in self.form_fields() ]
+            for field_name in field_names:
+                if field_name in form_fields:
+                    defaults[field_name] = getattr(customer, field_name)
+        
+        return defaults
         
     def __call__(self):
         main = get_template('templates/master.pt')
         return dict(request=self.request, main=main)
         
-    def handle_add(self, converted):
-        
-        customer = Customer()
-        # Apply schema fields to the flight object
+    def _apply_data(self, customer, converted):
+        # Apply schema fields to the customer object
         field_names = [ p.key for p in class_mapper(Customer).iterate_properties ]
         for field_name in field_names:
             if field_name in converted.keys():
                 setattr(customer, field_name, converted[field_name])
         
+    def handle_add(self, converted):
+        customer = Customer()
+        self._apply_data(customer, converted)
         session = DBSession()
         session.add(customer)
+        return HTTPFound(location=route_url('customers', self.request))
+        
+    def handle_submit(self, converted):
+        customer_id = self.request.matchdict['customer']
+        session = DBSession()
+        customer = session.query(Customer).filter_by(id=customer_id).one()
+        self._apply_data(customer, converted)
         return HTTPFound(location=route_url('customers', self.request))
         
     def handle_cancel(self):
