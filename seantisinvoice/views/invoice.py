ce3f196932ebb0df2fbcfa2d36f6aedcf0ddfbad
Tobias Reinhard <tobias.reinhard@seantis.ch>
2010-01-08 16:53:57 +0000

    Subform for invoice items.

--- seantisinvoice/views/invoice.py
+++ seantisinvoice/views/invoice.py
@@ -11,7 +11,16 @@ from repoze.bfg.url import route_url
 from repoze.bfg.chameleon_zpt import get_template
 
 from seantisinvoice.models import DBSession
-from seantisinvoice.models import CustomerContact, Invoice
+from seantisinvoice.models import CustomerContact, Invoice, InvoiceItem
+
+class InvoiceItemSchema(schemaish.Structure):
+    
+    item_id = schemaish.Integer()
+    service_description = schemaish.String()
+    amount = schemaish.Float()
+    hours = schemaish.Float()
+    
+invoice_item_schema = InvoiceItemSchema()
 
 class InvoiceSchema(schemaish.Structure):
     
@@ -22,8 +31,9 @@ class InvoiceSchema(schemaish.Structure):
     recurring_term = schemaish.Integer()
     payment_term = schemaish.Integer(validator=validator.Required())
     currency = schemaish.String(validator=validator.Required())
+    item_list = schemaish.Sequence(invoice_item_schema)
     
-schema = InvoiceSchema()
+invoice_schema = InvoiceSchema()
 
 class InvoiceController(object):
     
@@ -31,7 +41,7 @@ class InvoiceController(object):
         self.request = request
         
     def form_fields(self):
-        return schema.attrs
+        return invoice_schema.attrs
         
     def form_defaults(self):
         
@@ -48,10 +58,22 @@ class InvoiceController(object):
             except NoResultFound:
                 return HTTPFound(location = route_url('invoices', self.request))  
             field_names = [ p.key for p in class_mapper(Invoice).iterate_properties ]
-            form_fields = [ field[0] for field in self.form_fields() ]
+            form_fields = [ field[0] for field in invoice_schema.attrs ]
             for field_name in field_names:
                 if field_name in form_fields:
                     defaults[field_name] = getattr(invoice, field_name)
+                    
+            # Default values for the item subforms
+            defaults['item_list'] = []
+            for item in invoice.items:
+                item_defaults = {}
+                field_names = [ p.key for p in class_mapper(InvoiceItem).iterate_properties ]
+                form_fields = [ field[0] for field in invoice_item_schema.attrs ]
+                for field_name in field_names:
+                    if field_name in form_fields:
+                        item_defaults[field_name] = getattr(item, field_name)
+                item_defaults['item_id'] = item.id
+                defaults['item_list'].append(item_defaults)
         
         return defaults
         
@@ -63,6 +85,8 @@ class InvoiceController(object):
         for contact in session.query(CustomerContact).all():
             options.append((contact.id, '%s: %s %s' % (contact.customer.name, contact.first_name, contact.last_name)))
         widgets['customer_contact_id'] = formish.SelectChoice(options=options)
+        widgets['item_list'] = formish.SequenceDefault(min_start_fields=1)
+        widgets['item_list.*.item_id'] = formish.Hidden()
         
         return widgets
         
@@ -76,6 +100,29 @@ class InvoiceController(object):
         for field_name in field_names:
             if field_name in converted.keys():
                 setattr(invoice, field_name, converted[field_name])
+                
+        # Apply data of the items subforms
+        session = DBSession()
+        item_map = {}
+        for item in invoice.items:
+            item_map[item.id] = item
+        for item_data in converted['item_list']:
+            if item_data['item_id']:
+                item_id = item_data['item_id']
+                item = item_map[item_id]
+                del item_map[item_id]
+            else:
+                item = InvoiceItem()
+                item.invoice = invoice
+                session.add(item)
+            # Apply schema fields to the customer object
+            field_names = [ p.key for p in class_mapper(InvoiceItem).iterate_properties ]
+            for field_name in field_names:
+                if field_name in item_data.keys():
+                    setattr(item, field_name, item_data[field_name])
+        # Remove contact items that have been removed in the form
+        for item in item_map.values():
+            session.delete(item)
         
     def handle_add(self, converted):
         invoice = Invoice()
