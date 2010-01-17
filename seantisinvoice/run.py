470f94ab686b2fb9cecefe460a4d6cfd6aa68fac
Tobias Reinhard <tobias.reinhard@seantis.ch>
2010-01-17 11:49:41 +0000

    Not needed anymore.

--- seantisinvoice/run.py
+++ seantisinvoice/run.py
@@ -1,18 +1,8 @@
 from repoze.bfg.router import make_app
 
 import seantisinvoice
-from seantisinvoice.models import DBSession, RootFactory
+from seantisinvoice.models import RootFactory
 from seantisinvoice.models import initialize_sql
-
-class Cleanup:
-    def __init__(self, cleaner):
-        self.cleaner = cleaner
-    def __del__(self):
-        self.cleaner()
-
-def handle_teardown(event):
-    environ = event.request.environ
-    environ['seantisinvoice.sasession'] = Cleanup(DBSession.remove)
 
 def app(global_config, **settings):
     """ This function returns a repoze.bfg.router.Router object.
