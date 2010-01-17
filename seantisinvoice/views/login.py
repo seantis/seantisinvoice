68698cb8ac0a7cb97082bc0b97bcb06db6e30512
Tobias Reinhard <tobias.reinhard@seantis.ch>
2010-01-17 11:41:26 +0000

    Better login handler. Logout now also works on Safari.

--- seantisinvoice/views/login.py
+++ seantisinvoice/views/login.py
@@ -1,9 +1,20 @@
+from webob.exc import HTTPFound
+
 from repoze.bfg.chameleon_zpt import render_template_to_response 
+from repoze.bfg.security import authenticated_userid
 
 def view_login(request):
-    if 'repoze.who.logins' in request.environ:
+    if 'failed' in request.params:
         failed = True
     else:
         failed = False
     return render_template_to_response('templates/login.pt', request=request, login_failed=failed)
+    
+def view_login_redirect(request):
+    
+    user_id = authenticated_userid(request)
+    if user_id:
+        return HTTPFound(location = request.application_url)
+    else:
+        return HTTPFound(location = request.application_url + '/login?failed=1')  
     
