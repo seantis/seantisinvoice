069fa8d142394018aea60d038f6535fb7b06948b
Tobias Reinhard <tobias.reinhard@seantis.ch>
2010-01-19 16:24:57 +0000

    Clean up after request (in a more robust way than before).

--- seantisinvoice/run.py
+++ seantisinvoice/run.py
@@ -1,8 +1,19 @@
+import transaction
+
 from repoze.bfg.router import make_app
+from repoze.tm import after_end
+from repoze.tm import isActive
 
 import seantisinvoice
 from seantisinvoice.models import RootFactory
+from seantisinvoice.models import DBSession
 from seantisinvoice.models import initialize_sql
+
+def handle_teardown(event):
+    environ = event.request.environ
+    if isActive(environ):
+        t = transaction.get()
+        after_end.register(DBSession.remove, t)
 
 def app(global_config, **settings):
     """ This function returns a repoze.bfg.router.Router object.
