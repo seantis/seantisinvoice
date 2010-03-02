f653da317a2a28b9db1fdfa56322cf6972ac50e1
Fabian Reinhard <fabian.reinhard@seantis.ch>
2010-03-02 21:09:01 +0100

    work around to hide company name on pdf export if a person and not company is invoiced. Might be done in a nicer way!

--- seantisinvoice/views/invoice_export.py
+++ seantisinvoice/views/invoice_export.py
@@ -20,11 +20,18 @@ def view_invoice_pdf(request):
         return Response(status=404)
     
     rml_template = 'templates/rml/' + company.invoice_template
+    
+    # Hide company name if company name and contact name are one and the same
+    # We should find a nicer way to handle that. But it becomes a use case 
+    # when you need to invoice persons without company!
+    if invoice.contact.customer.name == (invoice.contact.first_name + ' ' + invoice.contact.last_name):
+        hide_company = True
+    else: hide_company = False
         
     # Only jpeg without PIL
     logo_path = os.path.join(os.path.dirname(__file__), 'templates', 'static', 'uploads', 'logo.jpg')
         
-    result = render_template(rml_template, invoice=invoice, logo_path=logo_path,
+    result = render_template(rml_template, invoice=invoice, logo_path=logo_path, hide_company=hide_company,
                              formatThousands=formatThousands)
     response = Response(parseString(result.encode('utf-8')).read())
     response.content_type =  "application/pdf"
